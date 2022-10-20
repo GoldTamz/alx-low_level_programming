63b286beda6ea4efe882999e0b095d08d21dec0c
GoldTamz <tamsakpanah@gmail.com>
2022-10-20 01:13:48 -0700

    this program prints _putcher followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,6 +3,7 @@
  *main-prints _putchar
  *
  *Return:(0)
+ *
  */
 int main(void)
 {	
@@ -15,5 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return ('0');
+	return('0');
 }
