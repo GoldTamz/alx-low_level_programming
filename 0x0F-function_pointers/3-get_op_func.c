d581d2b51596037093b01f99927f852595149c24
GoldTamz <tamsakpanah@gmail.com>
2022-11-16 20:29:27 -0800

    a program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,14 +17,14 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0
-
-		while (i < 10)
-		{
-			if (s[0] == ops->op[i])
-				break;
-			i++;
-		}
+	int i = 0;
+	
+	while (i < 10);
+	{
+		if (s[0] == ops->op[i])
+			break;
+		i++;
+	}
 
 	return (ops[i / 2].f);
 }
